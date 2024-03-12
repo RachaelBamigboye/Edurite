7f071d9ed9462c32ff27741dfe673f2b8adde17c
Rachael <odunayo845@gmail.com>
2024-03-12 16:44:42 +0100

    changed university to universities

--- src/component/About/About.jsx
+++ src/component/About/About.jsx
@@ -1,15 +1,15 @@
-import './About.css'
-import about_img from '../../assets/about.png'
+import "./About.css";
+import about_img from "../../assets/about.png";
 import play_icon from "../../assets/play-icon.png";
 const About = () => {
   return (
     <div className="about">
       <div className="about-left">
-        <img src={about_img} alt="" className='about-img'></img>
-        <img src={play_icon} alt="" className='play-icon'></img>
+        <img src={about_img} alt="" className="about-img"></img>
+        <img src={play_icon} alt="" className="play-icon"></img>
       </div>
       <div className="about-right">
-        <h3>ABOUT UNIVERSITY</h3>
+        <h3>ABOUT UNIVERSITIES</h3>
         <h2>Nurturing Tomorrow Leaders Today</h2>
         <p>
           Embark on a transformative educational journey with our universitys
@@ -17,7 +17,7 @@ const About = () => {
           designed to empower students with the knowledge, skills, and
           experiences needed to excel in the dynamic field of education.
         </p>
-        
+
         <p>
           With a focus on innovation, hands-on learning, and personalized
           mentorship, our programs prepare aspiring educators to make a
@@ -26,14 +26,13 @@ const About = () => {
 
         <p>
           Whether you aspire to become a teacher, administrator, counselor, or
-          educational leader, our diverse range of programs offers the perfect
+          educational leaders, our diverse range of programs offers the perfect
           pathway to achieve your goals and unlock your full potential in
           shaping the future of education.
         </p>
-
       </div>
     </div>
   );
-}
+};
 
-export default About
+export default About;
